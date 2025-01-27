92ae3d721eed9456f605adc6f5f68554d9a83cdc
Andrew Boob <AndrewB2277@outlook.com>
2025-01-27 12:46:00 -0500

    added vocab

--- src/components/NounDeclensions.js
+++ src/components/NounDeclensions.js
@@ -1,165 +1,61 @@
 import React from 'react'
 import "./NounDeclensions.css"
 
-export default function NounDeclensions() {
+import declensions from "../data/nounDeclensions.json"
+
+const cases = [
+  { id: 'nominative', label: 'Nominative' },
+  { id: 'genitive', label: 'Genitive' },
+  { id: 'dative', label: 'Dative' },
+  { id: 'accusative', label: 'Accusative' },
+  { id: 'vocative', label: 'Vocative'}
+]
+
+
+function NounTable({declension}) {
   return (
-    <div className='noun'>
-      <h1>Noun Declensions</h1>
-      <h2>1st Declension</h2>
+    <div>
+      <h2>{declension.title}</h2>
       <table className="noun-table">
         <thead>
           <tr>
             <th></th>
-            <th className="noun-header" colSpan={2}>Long alpha</th>
-            <th className="noun-header" colSpan={2}>Eta</th>
+            <th className="noun-header" colSpan={2}>{declension.variants[0].name}</th>
+            <th className="noun-header" colSpan={2}>{declension.variants[1].name}</th>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td>Singular</td>
-            <td>Plural</td>
-            <td>Singular</td>
-            <td>Plural</td>
-          </tr>
-          <tr>
-            <td>Nominative / Vocative</td>
-            <td>-ᾱ</td>
-            <td>-αι</td>
-            <td>-η</td>
-            <td>-αι</td>
-          </tr>
-          <tr>
-            <td>Genitive</td>
-            <td>-ᾱς</td>
-            <td>-ων</td>
-            <td>-ης</td>
-            <td>-ων</td>
-          </tr>
-          <tr>
-            <td>Dative</td>
-            <td>-ᾳ</td>
-            <td>-αις</td>
-            <td>-ῃ</td>
-            <td>-αις</td>
-          </tr>
-          <tr>
-            <td>Accusative</td>
-            <td>-ᾱν</td>
-            <td>-ας</td>
-            <td>-ην</td>
-            <td>-ας</td>
-          </tr>
-        </tbody>
-      </table>
-      <h2>2st Declension</h2>
-      <table className="noun-table">
-        <thead>
           <tr>
             <th></th>
-            <th className="noun-header" colSpan={2}>Masc. / Fem.</th>
-            <th className="noun-header" colSpan={2}>Neuter</th>
+            <th>Singular</th>
+            <th>Plural</th>
+            <th>Singular</th>
+            <th>Plural</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td>Singular</td>
-            <td>Plural</td>
-            <td>Singular</td>
-            <td>Plural</td>
-          </tr>
-          <tr>
-            <td>Nominative</td>
-            <td>-ος</td>
-            <td>-οι</td>
-            <td>-ον</td>
-            <td>-α</td>
-          </tr>
-          <tr>
-            <td>Genitive</td>
-            <td>-ου</td>
-            <td>-ων</td>
-            <td>-ου</td>
-            <td>-ων</td>
-          </tr>
-          <tr>
-            <td>Dative</td>
-            <td>-ῳ</td>
-            <td>-οις</td>
-            <td>-ῳ</td>
-            <td>-οις</td>
-          </tr>
-          <tr>
-            <td>Accusative</td>
-            <td>-ον</td>
-            <td>-ους</td>
-            <td>-ον</td>
-            <td>-α</td>
-          </tr>
-          <tr>
-            <td>Vocative</td>
-            <td>-ε</td>
-            <td>-οι</td>
-            <td>-ον</td>
-            <td>-α</td>
-          </tr>
-        </tbody>
-      </table>
-      <h2>3st Declension</h2>
-      <table className="noun-table">
-        <thead>
-          <tr>
-            <th></th>
-            <th className="noun-header" colSpan={2}>Masc./Fem.</th>
-            <th className="noun-header" colSpan={2}>Neuter</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td>Singular</td>
-            <td>Plural</td>
-            <td>Singular</td>
-            <td>Plural</td>
-          </tr>
-          <tr>
-            <td>Nominative</td>
-            <td>---</td>
-            <td>-ες</td>
-            <td>---</td>
-            <td>-α</td>
-          </tr>
-          <tr>
-            <td>Genitive</td>
-            <td>-ος</td>
-            <td>-ων</td>
-            <td>-ος</td>
-            <td>-ων</td>
-          </tr>
-          <tr>
-            <td>Dative</td>
-            <td>-ι</td>
-            <td>-σι(ν)</td>
-            <td>-ι</td>
-            <td>-σι(ν)</td>
-          </tr>
-          <tr>
-            <td>Accusative</td>
-            <td>-α / -ν</td>
-            <td>-ας</td>
-            <td>---</td>
-            <td>-α</td>
-          </tr>
-          <tr>
-            <td>Vocative</td>
-            <td>----</td>
-            <td>-ες</td>
-            <td>----</td>
-            <td>-α</td>
-          </tr>
+          {cases.map(({ id, label }) => (
+            <tr key={id}>
+              <td>{label}</td>
+              <td>{declension.variants[0].cases[id].singular}</td>
+              <td>{declension.variants[0].cases[id].plural}</td>
+              <td>{declension.variants[1].cases[id].singular}</td>
+              <td>{declension.variants[1].cases[id].plural}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   )
+}
+
+export default function NounDeclensions() {
+  const { first, second, third } = declensions;
+
+  return (
+    <div className='noun'>
+      <h1>Noun Declensions</h1>
+      <NounTable declension={first} />
+      <NounTable declension={second} />
+      <NounTable declension={third} />
+    </div>
+  );
 }
